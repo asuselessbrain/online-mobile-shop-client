430a5da05b8d9b8ee6abb0f7aad15f996b6baf42
asuselessbrain <shovon1023456789@gmail.com>
2024-07-01 15:39:19 +0600

    contact us page design and add map in contact page

--- src/pages/contactUs/ContactUs.jsx
+++ src/pages/contactUs/ContactUs.jsx
@@ -1,12 +1,131 @@
+import { useRef } from "react";
 import { Helmet } from "react-helmet";
+import emailjs from '@emailjs/browser';
+import { toast } from "react-toastify";
 
 const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_wz78s0d", "template_7ekj5j3", form.current, {
+        publicKey: "UVd1CvIEZyy0H99F0",
+      })
+      .then(
+        () => {
+          toast.success("Email send Successfully!")
+          
+        },
+        (error) => {
+          toast.error("Something went wrong!")
+        }
+      );      
+  };
+
   return (
     <div>
       <Helmet>
         <title>AstraGadgets | Contact Us</title>
       </Helmet>
-      This is contact page
+
+      <section className="text-gray-600 body-font relative">
+        {" "}
+        <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div className="lg:w-2/3 md:w-1/2 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+            <iframe className="absolute inset-0 w-full"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d909.971932187659!2d89.59294693030671!3d24.175669903507366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fdd9baf0c7c97f%3A0xe591ccec807ed2db!2z4Kau4KeH4Ka44Ka-4Kaw4KeN4Ka4IOCmueCmvuCmnOCngCDgpp_gp43gprDgp4fgpqHgpr7gprDgp43gprg!5e0!3m2!1sen!2sbd!4v1719825183592!5m2!1sen!2sbd"
+              width="600"
+              height="450"
+              style={{ border: "0" }}
+              allowfullscreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+            <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+              <div className="lg:w-1/2 px-6">
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  ADDRESS
+                </h2>
+                <p className="mt-1">
+                  Photo booth tattooed prism, portland taiyaki hoodie neutra
+                  typewriter
+                </p>
+              </div>
+              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  EMAIL
+                </h2>
+                <a className="text-red-500 leading-relaxed">
+                  example@email.com
+                </a>
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
+                  PHONE
+                </h2>
+                <p className="leading-relaxed">123-456-7890</p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+            <div className="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
+              <p className="text-base font-semibold uppercase tracking-wide text-blue-600 dark:text-blue-200">
+                Contact
+              </p>
+              <h2 className="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl">
+                Get in Touch
+              </h2>
+              <p className="mx-auto mt-4 max-w-3xl text-xl text-gray-600 dark:text-slate-400">
+                In hac habitasse platea dictumst
+              </p>
+            </div>
+            <form ref={form} onSubmit={sendEmail}>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="name"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="from_name"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="email"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="from_email"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="message"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                ></textarea>
+              </div>
+              <input className="text-white bg-red-500 border-0 w-full py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg" type="submit" value="Send" />
+            </form>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
