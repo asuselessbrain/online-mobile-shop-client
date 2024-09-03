7a961a487ea060c1cc7288b39f2fcdb91cb013ec
asuselessbrain <shovon1023456789@gmail.com>
2024-09-03 09:33:23 +0600

    quantity btn updated in my cart page

--- src/pages/dashboard/myCart/CartTableRow.jsx
+++ src/pages/dashboard/myCart/CartTableRow.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { MdDeleteSweep } from "react-icons/md";
 
 const CartTableRow = ({ item, index, handleRemoveToCart }) => {
-  console.log(item)
+  const [quantity, setQuantity] = useState(1);
+
+  const handleIncrement = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
+
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity((prevQuantity) => prevQuantity - 1);
+    }
+  };
   return (
     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
       <td className="py-4">
@@ -10,7 +21,7 @@ const CartTableRow = ({ item, index, handleRemoveToCart }) => {
       <td className="py-4">
         <img
           src={item.orderDetails.image}
-          className="w-16 md:w-32 mx-auto"
+          className="w-12 mx-auto"
           alt="productImage"
         />
       </td>
@@ -24,65 +35,69 @@ const CartTableRow = ({ item, index, handleRemoveToCart }) => {
         {item.orderDetails.category}
       </td>
       <td className="px-6 py-4">
-        <div className="flex items-center">
-          <button
-            className="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-            type="button"
+      <div className="flex items-center">
+        <button
+          type="button"
+          id="decrement-button-2"
+          onClick={handleDecrement}
+          className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+        >
+          <svg
+            className="h-2.5 w-2.5 text-gray-900 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 2"
           >
-            <span className="sr-only">Quantity button</span>
-            <svg
-              className="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 2"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h16"
-              />
-            </svg>
-          </button>
-          <div>
-            <input
-              type="number"
-              id="first_product"
-              className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="1"
-              required
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h16"
             />
-          </div>
-          <button
-            className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-            type="button"
+          </svg>
+        </button>
+        <input
+          type="text"
+          id="counter-input-2"
+          value={quantity}
+          readOnly
+          className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white"
+        />
+        <button
+          type="button"
+          id="increment-button-2"
+          onClick={handleIncrement}
+          className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+        >
+          <svg
+            className="h-2.5 w-2.5 text-gray-900 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
           >
-            <span className="sr-only">Quantity button</span>
-            <svg
-              className="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 18"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 1v16M1 9h16"
-              />
-            </svg>
-          </button>
-        </div>
-      </td>
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </button>
+      </div>
+    </td>
       <td className="px-6 py-4 font-semibold text-red-600 dark:text-red-600">
         $ {item.orderDetails.price}
       </td>
       <td className="px-6 py-4">
-        <MdDeleteSweep size={34} onClick={()=>handleRemoveToCart(item._id)} className="mx-auto text-red-600 cursor-pointer" />
+        <MdDeleteSweep
+          size={34}
+          onClick={() => handleRemoveToCart(item._id)}
+          className="mx-auto text-red-600 cursor-pointer"
+        />
       </td>
     </tr>
   );
