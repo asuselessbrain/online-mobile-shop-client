5d165be5359b1b207af0f19f2218a62cb000fd3b
asuselessbrain <shovon1023456789@gmail.com>
2024-09-06 18:17:34 +0600

    client side stripe integrated

--- src/pages/dashboard/payment/Payment.jsx
+++ src/pages/dashboard/payment/Payment.jsx
@@ -1,5 +1,10 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements, } from "@stripe/react-stripe-js";
+import PaymentForm from "./PaymentForm";
+
+const stripePromise = loadStripe('pk_test_51PvDzQP9iYm2H4Lsc4funaW5ZuBueH9knGBwkwJGMUXWiw0Iq9JENF2OVCqu0xhLNAv5R6vDUN5r7xZi5mZFdy9300Ozmn67cw');
 
 const Payment = ({ isOpen, closeModal }) => {
   return (
@@ -31,26 +36,14 @@ const Payment = ({ isOpen, closeModal }) => {
               <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <Dialog.Title
                   as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900"
+                  className="text-lg font-medium leading-6 text-gray-900 mb-6"
                 >
-                  Payment successful
+                  Pay Now
                 </Dialog.Title>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    Your payment has been successfully submitted. We’ve sent you
-                    an email with all of the details of your order.
-                  </p>
-                </div>
 
-                <div className="mt-4">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={closeModal}
-                  >
-                    Got it, thanks!
-                  </button>
-                </div>
+                <Elements stripe={stripePromise}>
+                  <PaymentForm />
+                </Elements>
               </Dialog.Panel>
             </Transition.Child>
           </div>
